2ad3019e1d9001907723fe6c3c0f2c333bccdd42
Zhura_is65 <muxmax969@gmail.com>
2023-05-22 09:37:21 +0300

    commit

--- Project7/main.cpp
+++ Project7/main.cpp
@@ -1,82 +1,32 @@
-//ex_1_
-#include <iostream>
+//ex_21_16.04.2023
+#include <iostream> 
 using namespace std;
-int main()
+void countPosNegZero(int* arr, int size, int* positive, int* negative, int* zero)
 {
-    int a, b, * ptr_a, * ptr_b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
-    ptr_a = &a;
-    ptr_b = &b;
-    if (*ptr_a > *ptr_b) {
-
-        cout << *ptr_a << " is the maximum number." << endl;
-    }
-    else
+    *positive = 0;
+    *negative = 0;
+    *zero = 0;
+    for (int i = 0; i < size; i++) 
     {
-        cout << *ptr_b << " is the maximum number." << endl;
-    }
-    return 0;
-}
 
-//ex_3_
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a, b;
-    std::cout << "Enter two numbers: ";
-    std::cin >> a >> b;
-    int* ptrA = &a;
-    int* ptrB = &b;
-    int temp = *ptrA;
-    *ptrA = *ptrB;
-    *ptrB = temp;
-    std::cout << "Swapped numbers: " << a << " " << b << std::endl;
-    return 0;
-}
+        if (*(arr + i) > 0) {
 
-//ex_4_
-#include <iostream>
-using namespace std;
-int main() {
-    int a, b;
-    char op;
-    std::cout << "Enter an operation (+, -, *, /): ";
-    std::cin >> op;
-    std::cout << "Enter two numbers: ";
-    std::cin >> a >> b;
-    int* ptrA = &a;
-    int* ptrB = &b;
-    int result;
-    switch (op)
-    {
-    case '+':
-        result = *ptrA + *ptrB;
-        std::cout << "Result: " << result << std::endl;
-        break;
-    case '-':
-        result = *ptrA - *ptrB;
-        std::cout << "Result: " << result << std::endl;
-        break;
-    case '*':
-        result = *ptrA * *ptrB;
-        std::cout << "Result: " << result << std::endl;
-        break;
-    case '/':
-        if (*ptrB == 0)
-        {
-            std::cout << "Error: division by zero." << std::endl;
+            (*positive)++;
+
         }
-        else
+        else if (*(arr + i) < 0) 
         {
-            result = *ptrA / *ptrB;
-            std::cout << "Result: " << result << std::endl;
+
+            (*negative)++;
+
         }
-        break;
-    default:
-        std::cout << "Invalid operation." << std::endl;
-        break;
+        else 
+        {
+
+            (*zero)++;
+
+        }
+
     }
     return 0;
 }
